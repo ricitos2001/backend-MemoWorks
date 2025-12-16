88c3148c2e7afde4b595a35167ffd47584dc33cd
ricitos2001 <cesar2001ricitos@gmail.com>
2025-12-16 20:50:26 +0100

    cambios en backend

--- src/main/java/com/example/catalog/domain/entities/Task.java
+++ src/main/java/com/example/catalog/domain/entities/Task.java
@@ -19,7 +19,7 @@ public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private String title;
     @Column(nullable = false)
     private String description;
