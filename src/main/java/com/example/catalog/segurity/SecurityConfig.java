53fe8d3d3151011735d8e3743e3b4755fe9d5f98
ricitos2001 <cesar2001ricitos@gmail.com>
2025-12-27 18:45:20 +0100

    cambios en el backend

--- src/main/java/com/example/catalog/segurity/SecurityConfig.java
+++ src/main/java/com/example/catalog/segurity/SecurityConfig.java
@@ -77,7 +77,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("https://frontend-memoworks.onrender.com"));
+        config.setAllowedOrigins(List.of("https://memoworks.onrender.com"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
